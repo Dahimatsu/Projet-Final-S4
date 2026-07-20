14203a9efd8c4b126352d8c5a0dea5ba6ece4586
joshua-YHW <rakotobejoshua25@gmail.com>
2026-07-20 09:46:40 +0300

    uptade route

--- app/Controllers/OperationController.php
+++ app/Controllers/OperationController.php
@@ -18,7 +18,7 @@ class OperationController extends BaseController
     }
 
     public function vueDepot() {
-        return view('client/depot');
+        return view('front-office/depot');
     }
     public function depot() {
         $session = session();
@@ -47,7 +47,7 @@ class OperationController extends BaseController
 
 
     public function vueRetrait() {
-        return view('client/retrait');
+        return view('front-office/retrait');
     }
     public function retrait() {
         $session = session();
@@ -88,7 +88,7 @@ class OperationController extends BaseController
     }
 
     public function vueTransfert() {
-        return view('client/transfert');
+        return view('front-office/transfert');
     }
     public function transfert() {
         $session = session();
