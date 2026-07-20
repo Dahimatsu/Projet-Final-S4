e7d16d42a782d9a9596a6aa94010498f36322cd5
joshua-YHW <rakotobejoshua25@gmail.com>
2026-07-20 07:41:24 +0300

    login Client

--- app/Controllers/AuthClientController.php
+++ app/Controllers/AuthClientController.php
@@ -13,22 +13,51 @@ class AuthClientController extends BaseController
         return view('client/login');
     }
 
+    public function firstAutenticate() {
+        $session = session();
+        $model = new ClientModel();
+
+        $numeroTelephone = $session->get('numero_telephone');
+        $nom = $this->request->getPost('nom');
+        $prenom = $this->request->getPost('prenom');
+        $data = [
+            'numero_telephone' => $numeroTelephone,
+            'nom'              => $nom,
+            'prenom'           => $prenom
+        ];
+        $model->insert($data);
+        $client = $model->where('numero_telephone', $numeroTelephone)->first();
+
+        $session->set([
+            'numero_telephone' => $client['numero_telephone'],
+            'nom' => $client['nom'],
+            'prenom' => $client['prenom']
+        ]);
+
+        return redirect()->to('/client/dashboard');
+    }
+
     public function authenticate() {
+        $session = session();
         $model = new ClientModel();
         $numeroTelephone = $this->request->getPost('numero_telephone');
         
         if($model->clientExiste('numero_telephone', $numeroTelephone)) {
             $client = $model->where('numero_telephone', $numeroTelephone)->first();
-            $session = session();
             $session->set([
                 'numero_telephone' => $client['numero_telephone'],
                 'nom' => $client['nom'],
                 'prenom' => $client['prenom']
             ]);
             return redirect()->to('/client/dashboard');
+        }else{
+            $session->set([
+                'numero_telephone' => $numeroTelephone
+            ]);
         }
         return redirect()->back()->with('notExist', 'Entrez vos nom et prénom.s');
     }
+
 
     public function logout() {
         session()->destroy();
